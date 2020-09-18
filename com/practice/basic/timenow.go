6e82b149b3cfcafc16946013e5703e3e74c32bff
Gaurav <gaurav9822@gmail.com>
2020-09-19 02:00:36 +0530

    Day 2: Code for current time.

---
+++ com/practice/basic/timenow.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Current time:", time.Now())
+}
+
+//Output - Current time: 2020-09-19 01:59:21.5280986 +0530 IST m=+0.006998201
