d90a81bc9c729ffca84b89e19441c30a08fde37f
Gaurav <gaurav9822@gmail.com>
2020-10-04 00:50:02 +0530

    Day 14: For loop

---
+++ com/practice/basic/for-loop.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	//There is no while loop.
+	//No parentheses and braces '{ }' are required.
+	for i := 0; i < 5; i++ {
+		fmt.Println("Index: ", i)
+	}
+}
